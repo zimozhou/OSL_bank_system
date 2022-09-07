a71dc3633eaf376cb7b48f2cc83390b4e7991a59
zimozhou <2636453880@qq.com>
2022-09-08 01:24:39 +0800

    second commit

--- test.py
+++ test.py
@@ -5,12 +5,12 @@ account_creation("zimo", "USD")
 money_deposit("zimo", "USD", 300)
 money_withdrawal("zimo", "USD", 200)
 
-account_creation("zimo1", "USD")
-money_deposit("zimo1", "USD", 300)
-money_transfer("zimo", "zimo1", "USD", 50)
+account_creation("yuhan", "USD")
+money_deposit("yuhan", "USD", 300)
+money_transfer("zimo", "yuhan", "USD", 50)
 
 list_bank_account_balance("zimo")
-list_bank_account_balance("zimo1")
+list_bank_account_balance("yuhan")
 
 display_transaction_statement("zimo")
-display_transaction_statement("zimo1")
+display_transaction_statement("yuhan")
